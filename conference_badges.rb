5c84956555fc533a8124d018185a6ee0b37bc31d
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-24 00:00:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,10 +23,5 @@ end
 def printer(attendees) 
   badges_and_room_assignments.each_line do |line|
   puts line.chomp 
-  #batch_badge_creator(attendees) 
-#   puts badges.first
-#   assign_rooms(attendees)
-#   attendee = attendees.first 
-# puts "Hello, my name is #{attendee}."
   end 
 end 
